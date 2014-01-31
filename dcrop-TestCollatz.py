e65f79e042bbb59fff5a04422a701d54bce1b0d0
KyleCrop <kylecrop@utexas.edu>
2014-01-30 20:39:12 -0600

    My unit and acceptance tests.

---
+++ dcrop-TestCollatz.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+# -------------------------------
+# projects/collatz/TestCollatz.py
+# Copyright (C) 2014
+# Glenn P. Downing
+# -------------------------------
+
+"""
+To test the program:
+    % python TestCollatz.py >& TestCollatz.out
+    % chmod ugo+x TestCollatz.py
+    % TestCollatz.py >& TestCollatz.out
+"""
+
+# -------
+# imports
+# -------
+
+import io
+import unittest
+
+from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
+
+# -----------
+# TestCollatz
+# -----------
+
+class TestCollatz (unittest.TestCase) :
+    # ----
+    # read
+    # ----
+
+    def test_read (self) :
+        r = io.StringIO("1 10\n")
+        a = [0, 0]
+        b = collatz_read(r, a)
+        i, j = a
+        self.assertTrue(b == True)
+        self.assertTrue(i ==  1)
+        self.assertTrue(j == 10)
+
+    def test_read_2 (self) :
+        r = io.StringIO("")
+        a = [0, 0]
+        b = collatz_read(r, a)
+        i, j = a
+        self.assertTrue(b == False)
+
+    def test_read_3 (self) :
+        r = io.StringIO("1    10")
+        a = [0, 0]
+        b = collatz_read(r, a)
+        i, j = a
+        self.assertTrue(b == True)
+        self.assertTrue(i ==  1)
+        self.assertTrue(j == 10)
+
+    # ----
+    # eval
+    # ----
+
+    def test_eval_1 (self) :
+        v = collatz_eval(1, 10)
+        self.assertTrue(v == 20)
+
+    def test_eval_2 (self) :
+        v = collatz_eval(100, 200)
+        self.assertTrue(v == 125)
+
+    def test_eval_3 (self) :
+        v = collatz_eval(201, 210)
+        self.assertTrue(v == 89)
+
+    def test_eval_4 (self) :
+        v = collatz_eval(900, 1000)
+        self.assertTrue(v == 174)
+
+    def test_eval_5 (self) :
+        v = collatz_eval(300,    85)
+        self.assertTrue(v == 128)
+
+    # -----
+    # print
+    # -----
+
+    def test_print (self) :
+        w = io.StringIO()
+        collatz_print(w, 1, 10, 20)
+        self.assertTrue(w.getvalue() == "1 10 20\n")
+
+    def test_print_2 (self) :
+        w = io.StringIO()
+        collatz_print(w, "300", "   85", 128)
+        self.assertTrue(w.getvalue() == "300    85 128\n")
+
+    # -----
+    # solve
+    # -----
+
+    def test_solve (self) :
+        r = io.StringIO("1 10\n100 200\n201 210\n900 1000\n")
+        w = io.StringIO()
+        collatz_solve(r, w)
+        self.assertTrue(w.getvalue() == "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2 (self) :
+        r = io.StringIO("1     10\n300   85\n    210 201\n")
+        w = io.StringIO()
+        collatz_solve(r, w)
+        self.assertTrue(w.getvalue() == "1 10 20\n300 85 128\n210 201 89\n")
+
+# ----
+# main
+# ----
+
+print("TestCollatz.py")
+unittest.main()
+print("Done.")
